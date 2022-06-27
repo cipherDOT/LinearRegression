382dbba87726abda7e63cf9a13da271e585621de
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-06-27 19:23:50 +0530

    added plot dark mode

--- netutils.py
+++ netutils.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 
+plt.style.use('dark_background')
+
 def graph(x, y, model):
     predictions = [model.predict(i) for i in x]
     plt.scatter(x, y, color = "m",marker = "o", s = 30)
