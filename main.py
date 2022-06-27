26aea40ce40ab74b12075f0ea66b72789897a48f
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-06-27 19:32:42 +0530

    code comments

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+
+# importing the model class and the utility functions
 from regressor import LinearRegression
 from netutils import graph
 
+# main process
 if __name__ == "__main__":
+
+    # x and y data
     x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     y = [1, 2, 4, 7, 8, 9, 13, 16, 19, 20]
 
+    # linear regression model object instance
     regress = LinearRegression(x, y)
+
+    # training phase
     regress.train()
+
+    # print out the slope and intercept of the regression line
     regress.stats()
     
+    # plot the regression graph
     graph(x, y, regress)
