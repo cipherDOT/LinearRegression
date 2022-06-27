f4ec1e600764225a3a70ce0fb1fde9a002d7c3a3
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-06-27 19:32:13 +0530

    added comments

--- netutils.py
+++ netutils.py
@@ -1,9 +1,16 @@
+
+# import the visualization library (matplotlib in this case)
 from matplotlib import pyplot as plt
 
+# applying dark background
 plt.style.use('dark_background')
 
 def graph(x, y, model):
+
+    # get predictions of all data
     predictions = [model.predict(i) for i in x]
+
+    # plot code
     plt.scatter(x, y, color = "m",marker = "o", s = 30)
     plt.plot(x, predictions, color='b')
     plt.show()
